dc81004befbba4a0117f5829d97638436a6d1012
oceansalle <oceanesalleiae@gmail.com>
2022-04-13 15:14:49 +0200

    NavBar section

--- hotel-website/src/App.js
+++ hotel-website/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
+      <Router>
       <NavBar />
+      <Routes>
+        <Route path='/'></Route>
+      </Routes>
+      </Router>
+      
       
     </div>
   );
--- hotel-website/src/components/NavBar/NavBar.js
+++ hotel-website/src/components/NavBar/NavBar.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
-function NavBar() {
+const NavBar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
-    <div>
-      NavBar
-    </div>
-  );
-}
+    <>
+      <nav className="navbar">
+        <div className="container flex_space">
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
 
-export default NavBar; 
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li>
+              {" "}
+              <Link to="/" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/gallery" onClick={closeMobileMenu}>
+                Gallery
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/destination" onClick={closeMobileMenu}>
+                Destination
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/blog" onClick={closeMobileMenu}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/testimonial" onClick={closeMobileMenu}>
+                Testimonial{" "}
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/contact us" onClick={closeMobileMenu}>
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+
+          <div className="login-area flex">
+            <li>
+              <Link to="sign-in">
+                <i className="far fa-chevron-right">Sign in</i>
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <i className="far fa-chevron-right">Register</i>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact">
+                <button className="primary-btn">Request a Quote</button>
+              </Link>
+            </li>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
